5838fb564970c0fde35d8d3baee0fba1794be47d
Chris Kelley <chriskelley85@gmail.com>
2022-02-11 15:18:24 -0600

    2 tests

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,15 @@
-// Write your tests here
+import React from "react"
+import { render } from "@testing-library/react"
+import AppClass from "./AppClass"
+import AppFunctional from "./AppFunctional"
+
 test('sanity', () => {
   expect(true).toBe(false)
 })
+test("1", () => {
+  render(<AppClass/>);
+});
+
+test("2", () => {
+  render(<AppFunctional/>);
+});
